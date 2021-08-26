808615a0dda6a5c280da177a7d94c2f70e7366f5
Manav Lakhanpal <67679350+ML2712@users.noreply.github.com>
2021-08-26 12:12:48 +0530

    Update settings.py

--- edgeledger/settings.py
+++ edgeledger/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7+t%vz8mul3uh3-3n$vyruowya)_9tll^9v7_e1_kj%8wfrvw=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.techavm.herokuapp.com', 'techavm.herokuapp.com']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
